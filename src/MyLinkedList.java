5eebb8bdcf319a3bd574ad44446094eb698b2887
Nazheka <akezhanstudy@gmail.com>
2024-04-28 23:41:34 +0500

    fixed add for MyLinkedList

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -82,20 +82,19 @@ public class MyLinkedList<T> implements MyList<T>{
             current.next.previous = newNode;
             current.next = newNode;
         }
+        size++;
     }
 
     @Override
     public void addFirst(T item) {
         checkIndex(0);
         add(0, item);
-        size++;
     }
 
     @Override
     public void addLast(T item) {
         checkIndex(0);
         add(item);
-        size++;
     }
 
     @Override
