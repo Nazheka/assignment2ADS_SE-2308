4321b4a516eb1cee526cc232c7b6fd2ef64ae533
Nazheka <akezhanstudy@gmail.com>
2024-04-28 21:16:03 +0500

    made some methods for MyLinkedList

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,11 +1,147 @@
-public class MyLinkedList {
+import java.util.Iterator;
+
+public class MyLinkedList<T> implements MyList<T>{
     private class MyNode<T> {
         T data;
-        MyNode next;
-
+        MyNode<T> next;
+        MyNode<T> previous;
         public MyNode(T data) {
             this.data = data;
-            this.next = null;
+            next = null;
+            previous = null;
+        }
+    }
+    private MyNode<T> head;
+    private MyNode<T> tail;
+    private int size;
+
+    public MyLinkedList(){
+        head = null;
+        tail = null;
+        size = 0;
+    }
+    @Override
+    public void add(T item) {
+        MyNode<T> newNode = new MyNode<>(item);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            MyNode<T> currentNode = head;
+            while (currentNode.next != null){
+                currentNode = currentNode.next;
+            }
+            currentNode.next = newNode;
+            newNode.previous = currentNode;
+        }
+        size++;
+    }
+
+    @Override
+    public void set(int index, T item) {
+       checkIndex(index);
+       MyNode<T> currentNode = head;
+        for (int i = 0; i < index; i++) {
+            currentNode = currentNode.next;
+        }
+        currentNode.data = item;
+    }
+
+    public void printList() {
+        MyNode<T> currentNode = head;
+        for (int i = 0; i < size; i++) {
+            System.out.print(currentNode.data + " ");
+            currentNode = currentNode.next;
+        }
+    }
+
+    @Override
+    public void add(int index, T item) {
+
+    }
+
+    @Override
+    public void addFirst(T item) {
+
+    }
+
+    @Override
+    public void addLast(T item) {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T getFirst() {
+        return null;
+    }
+
+    @Override
+    public T getLast() {
+        return null;
+    }
+
+    @Override
+    public void remove(int index) {
+
+    }
+
+    @Override
+    public void removeFirst() {
+
+    }
+
+    @Override
+    public void removeLast() {
+
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+    @Override
+    public int indexOf(Object object) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object object) {
+        return 0;
+    }
+
+    @Override
+    public boolean exists(Object object) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size)  {
+            throw new IndexOutOfBoundsException("Index " + index + " is out of bounds");
         }
     }
 }
